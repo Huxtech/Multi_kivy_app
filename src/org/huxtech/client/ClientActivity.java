371a28014018fc0a941093bfa29b1eb48b13bf52
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-04-23 20:39:07 +0530

    Add activity to hold new app

---
+++ src/org/huxtech/client/ClientActivity.java
@@ -0,0 +1,60 @@
+package org.huxtech.client;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import org.kivy.android.PythonActivity;
+
+public class ClientActivity extends PythonActivity {
+    private static final String TAG = "ClientActivity";
+
+    private static ClientActivity instance;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "ClientActivity started");
+        super.onCreate(savedInstanceState);
+        instance = this;
+    }
+
+    public static ClientActivity getInstance() {
+        return instance;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (instance == this) {
+            instance = null;
+        }
+    }
+
+    @Override
+    public String getEntryPoint(String searchDir) {
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            String path = uri.getPath();
+            Log.i(TAG, "Will launch entrypoint from URI: " + path);
+            return path;
+        } else {
+            Log.w(TAG, "No entrypoint URI passed.");
+            finish();
+        }
+        return super.getEntryPoint(searchDir);
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        Log.d(TAG, "dispatchKeyEvent: " + keyCode);
+
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            Log.d(TAG, "BACK key intercepted!");
+            finish();
+            return true;
+        }
+        return super.dispatchKeyEvent(event);
+    }
+}
