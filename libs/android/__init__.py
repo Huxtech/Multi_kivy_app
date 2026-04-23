a501f4e0f533138373f98d3172def95c0f76c905
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2026-04-23 21:10:40 +0530

    Add laucnher and main.py

---
+++ libs/android/__init__.py
@@ -0,0 +1,21 @@
+import os
+from jnius import autoclass
+from android import mActivity # type: ignore
+from android.runnable import run_on_ui_thread  # type: ignore
+
+ClientActivity = autoclass("org.huxtech.client.ClientActivity")
+Intent = autoclass("android.content.Intent")
+Uri = autoclass("android.net.Uri")
+
+activity = mActivity
+AppStorageDir = os.path.join(mActivity.getFilesDir().getAbsolutePath(), "app")
+
+
+@run_on_ui_thread
+def launch_client_activity(entrypoint_path: str) -> None:
+    uri = Uri.parse("file://" + entrypoint_path)
+
+    intent = Intent(mActivity.getApplicationContext(), ClientActivity)
+    intent.setData(uri)
+    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+    mActivity.startActivity(intent)
--- main.py
+++ main.py
@@ -1,13 +1,42 @@
-from app import Main
-from jnius import autoclass
+import os
 
-activity = autoclass("org.kivy.android.PythonActivity").mActivity
-intent = activity.getIntent()
-entrypoint = intent.getStringExtra("entrypoint")
+from kivy import platform
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
 
-if __name__ == '__main__':
-    if entrypoint:
-        import runpy
-        runpy.run_path(entrypoint, run_name="__main__")
-    else:
-        Main().run()
+if platform == "android":
+    from libs.android import launch_client_activity, AppStorageDir
+
+
+AppKV = """
+Screen:
+    Button:
+        text: "Launch"
+        size_hint: None, None
+        height: dp(48)
+        width: dp(128)
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+        on_press:
+
+"""
+
+
+class Main(App):
+
+    def __init__(self, **kwargs):
+        Window.clearcolor = [1, 1, 1, 1]
+        super(Main, self).__init__(**kwargs)
+
+    def build(self):
+        return Builder.load_string(AppKV)
+
+    def launch(self, *args) -> None:
+        if platform == "android":
+            launch_client_activity(
+                os.path.abspath(os.path.join(AppStorageDir, "nested_app.py"))
+            )
+
+
+if __name__ == "__main__":
+    Main().run()
